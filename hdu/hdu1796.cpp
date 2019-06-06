51aa101de514e715c7f164c681b33267b2716199
yuzhechuan <yuzhechuan@qq.com>
2019-06-06 19:30:54 +0800

    中国剩余定理

---
+++ hdu/hdu1796.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+int m,rm;
+ll ans,n,a[15];
+inline ll gcd(const ll &x,const ll &y){
+    if(!y) return x;
+    return gcd(y,x%y);
+}
+inline void dfs(const int &x,const bool &flag,ll lcm){
+    lcm=a[x]/gcd(a[x],lcm)*lcm;
+    if(flag) ans+=(n-1)/lcm;
+    else ans-=(n-1)/lcm;
+    for(int i=x+1;i<=rm;i++) dfs(i,!flag,lcm);
+}
+signed main(){
+     while(~scanf("%lld%d",&n,&m)){
+         ans=rm=0;
+         for(int i=1;i<=m;i++){
+             scanf("%lld",&a[i]);
+             if(a[i]) a[++rm]=a[i];
+         }
+         for(int i=1;i<=rm;i++) dfs(i,1,a[i]);
+         printf("%lld\n",ans);
+     }
+}
